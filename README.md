e9c058f6ae988e3d45d6db498379dad5930590d6
Christian d'Heureuse <chdh@inventec.ch>
2023-12-05 03:22:27 +0100

    Joomla plugin overhauled

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A Joomla-MediaWiki authentication bridge.
 
 This project consists of two parts:
 
-* `User - MediaWiki Login`: A Joomla plugin which sets a cookie within the users web browser.
+* `MediaWiki Login`: A Joomla plugin which sets a cookie in the users web browser.
 * `AuthJoomla2023`: A MediaWiki extension which reads the cookie and automatically logs the user in.
 
 ## Notes
@@ -13,7 +13,7 @@ A valid Joomla session allows access to the wiki, without having to log in into 
 A wiki user profile is automatically created for the user on the first visit.
 
 This project is a successor of [AuthJoomla2](https://www.mediawiki.org/wiki/Extension:AuthJoomla2) by [Harold Prins](https://www.haroldprins.nl/).
-The Joomla extension is mostly unchanged, but the MediaWiki extension is a complete rewrite by
+The Joomla extension has been strongly simplified and the MediaWiki extension is a complete rewrite by
 [Christian d'Heureuse](https://www.inventec.ch/chdh) in 2023.
 
 The configuration parameters are mostly the same as in AuthJoomla2.
@@ -25,13 +25,8 @@ this zip file as a Joomla extension.
 
 Configure the following parameters in the Joomla backend under "Extensions" / "Plugins".
 
-* Mediawiki Secret Word: (choose your own secret word)
-* Mediawiki Cookie Domain: empty
-* Mediawiki Cookie Prefix: database name + "_mw_"
-* Mediawiki Cookie Path: "/"
-* Check New Usernames: off
-* Username Regex: empty
-* Regex Error Message: empty
+* Secret Word for cookie crypto check: (choose your own secret word)
+* Mediawiki cookie subdomain: empty if your Wiki uses the same domain (host) name as your Joomla, otherwise the name of the Wiki subdomain
 
 ## MediaWiki configuration
 
--- joomla/mediawikilogin.php
+++ joomla/mediawikilogin.php
@@ -1,181 +1,47 @@
 <?php
-/**
- * File: mediawikilogin.php
- * Op basis van: docuwiki bridge
- * Version: 2.4
- * Author docuwiki bridge: Tim Hewitt
- * Aangepast door: Harold Prins
- * Email: info@haroldprins.nl
- * Website: http://www.haroldprins.nl
- * Plugin for automatic Mediawiki login with Joomla.
- * Requires the Joomla authorization module for Mediawiki
- */
 
-// Check to ensure this file is included in Joomla!
-defined('_JEXEC') or die( 'Restricted access' );
+defined('_JEXEC') or die;
 
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\User\User;
+use Joomla\CMS\User\UserHelper;
 
-jimport('joomla.plugin.plugin');
+class PlgUserMediawikiLogin extends CMSPlugin {
 
-class plgUserMediawikiLogin extends JPlugin {
+   protected $app;
 
-    // function plgUserMediawikiLogin(& $subject, $config)
-    function __construct(& $subject, $config)
-    {
-        parent::__construct($subject, $config);
-    }
+   function onUserLogin (/*JAuthenticationResponse*/ $userAuthResponse, $options) {
 
+      if (!$this->app->isClient('site')) {
+         return true; }
 
-    function onUserBeforeSave($user, $isnew, $new)  {
-        $execution = $this->params->get('mediawiki_username', false);
-        if($execution  && $new) {
-            $regex = $this->params->get('mediawiki_usernameregex', '/^[a-zA-Z0-9- ]+$/');
-            $message = JText::_($this->params->get('mediawiki_usernameerrormessage', ''));
-            if ( preg_match ($regex, $new['username'] )) {
-                return true;
-            } else {
-                throw new \Exception($message);
-                return false;
-            }
-        }
-        return true;
-    }
+      $userId = (int)UserHelper::getUserId($userAuthResponse['username']);
+      if (!$userId) {
+         return true; }
+      $user = User::getInstance();
+      if (!$user->load($userId)) {
+         return true; }
+      $username = $user->username;
 
-    function onUserLogin($user, $options = array())
-    {
+      $mediawikiDomain = $this->params->get('mediawiki_cdomain');
+      $cookieName = 'MW'.md5('Joomla');
+      $key = $this->params->get('mediawiki_salt');
+      $checksum = MD5($username . $key);
+      $cookie = base64_encode("$username|$checksum");
+      $time = mktime(0, 0, 0, 12, 31, date('Y') + 10);
+      setcookie($cookieName, $cookie, $time, '/', $mediawikiDomain);
 
-        jimport('joomla.user.helper');
+      return true; }
 
-        $instance = $this->_getUser($user, $options);
+   function onUserLogout ($credentials) {
 
-        // If _getUser returned an error, then pass it back.
-        if (JError::isError($instance)) {
-            return false;
-        }
+      if (!$this->app->isClient('site')) {
+         return true; }
 
-        // If the user is blocked, redirect with an error
-        if ($instance->get('block') == 1) {
-            JError::raiseWarning('SOME_ERROR_CODE', JText::_('JERROR_NOLOGIN_BLOCKED'));
-            return false;
-        }
+      $mediawikiDomain = $this->params->get('mediawiki_cdomain');
+      $cookieName = 'MW'.md5('Joomla');
+      setcookie($cookieName, '', 1, '/', $mediawikiDomain);  // delete the cookie
 
-        $success = true;
+      return true; }
 
-        // If we are logged in to administrator back end, just exit
-        $app = JFactory::getApplication();
-        if ($app->isAdmin()) {
-            return ($success);
-        }
-
-        // actual Mediawiki directory for the cookie
-        $mediawiki_rel = $this->params->get('mediawiki_cpath');
-        $mediawiki_dom = $this->params->get('mediawiki_cdomain');
-
-        // Create the login cookie
-        $mediawiki_cookie = 'MW'.md5('Joomla');
-
-        $username = $user['username'];
-        $userid = intval(JUserHelper::getUserId($username));
-        $loggeduser = JFactory::getUser($userid); //Bug using different caps username fixed line 68 and 69, thanks to Duran
-        $username = $loggeduser->username;
-
-        $salt = $this->params->get('mediawiki_salt');
-        $username_match = MD5($username . $salt);
-
-        $cookie = base64_encode("$username|$username_match");
-        $mainframe = JFactory::getApplication();
-        // $time = time() + (60 * $mainframe->getCfg('lifetime')); //in te stellen via configuration.php
-        // patch chdh 2013-10-31:
-        $time = mktime(0, 0, 0, 12, 31, 2029);
-
-        $success = setcookie($mediawiki_cookie,$cookie,$time,$mediawiki_rel,$mediawiki_dom);
-
-        return $success;
-    }
-
-    function onUserLogout($user, $options = array())
-    {
-
-        $success = true;
-
-        // If we are logged in to administrator back end, just exit
-        $app = JFactory::getApplication();
-        if ($app->isAdmin()) {
-            return ($success);
-        }
-
-        // Delete the login cookie
-        $mediawiki_cookie = 'MW'.md5('Joomla');
-            $time = time() - 3600; //one hour ago
-
-
-            // delete the user's identifying cookies
-            $mediawiki_cprefix = $this->params->get('mediawiki_cprefix');
-            $mediawiki_cpath = $this->params->get('mediawiki_cpath');
-            $mediawiki_cdomain = $this->params->get('mediawiki_cdomain');
-
-            setcookie( $mediawiki_cookie,0,$time,$mediawiki_cpath, $mediawiki_cdomain);
-        setcookie( $mediawiki_cprefix . '_session', '', $time, $mediawiki_cpath, $mediawiki_cdomain);
-        setcookie( $mediawiki_cprefix . 'UserName', '', $time, $mediawiki_cpath, $mediawiki_cdomain);
-        setcookie( $mediawiki_cprefix . 'UserID',   '', $time, $mediawiki_cpath, $mediawiki_cdomain);
-        setcookie( $mediawiki_cprefix . 'Token',    '', $time, $mediawiki_cpath, $mediawiki_cdomain);
-
-        // Remember when the user logged out, to prevent seeing cached pages
-        $ts_now = gmdate( 'YmdHis', time() ); // emulates wfTimestampNow()
-        setcookie( $mediawiki_cprefix . 'LoggedOut', $ts_now, time() + 86400, $mediawiki_cpath, $mediawiki_cdomain);
-
-        return $success;
-    }
-
-    /**
-     * This method will return a user object
-     *
-     * If options['autoregister'] is true, if the user doesn't exist yet he will be created
-     *
-     * @param   array   $user       Holds the user data.
-     * @param   array   $options    Array holding options (remember, autoregister, group).
-     *
-     * @return  object  A JUser object
-     * @since   1.5
-     */
-    protected function &_getUser($user, $options = array())
-    {
-        $instance = JUser::getInstance();
-        if ($id = intval(JUserHelper::getUserId($user['username'])))  {
-            $instance->load($id);
-            return $instance;
-        }
-
-        //TODO : move this out of the plugin
-        jimport('joomla.application.component.helper');
-        $config = JComponentHelper::getParams('com_users');
-        // Default to Registered.
-        $defaultUserGroup = $config->get('new_usertype', 2);
-
-        $acl = JFactory::getACL();
-
-        $instance->set('id'         , 0);
-        $instance->set('name'           , $user['fullname']);
-        $instance->set('username'       , $user['username']);
-        $instance->set('password_clear' , $user['password_clear']);
-        $instance->set('email'          , $user['email']);  // Result should contain an email (check)
-        $instance->set('usertype'       , 'deprecated');
-        $instance->set('groups'     , array($defaultUserGroup));
-
-        //If autoregister is set let's register the user
-        $autoregister = isset($options['autoregister']) ? $options['autoregister'] :  $this->params->get('autoregister', 1);
-
-        if ($autoregister) {
-            if (!$instance->save()) {
-                return JError::raiseWarning('SOME_ERROR_CODE', $instance->getError());
-            }
-        }
-        else {
-            // No existing user and autoregister off, this is a temporary user.
-            $instance->set('tmp_user', true);
-        }
-
-        return $instance;
-    }
-
-}
+   }
